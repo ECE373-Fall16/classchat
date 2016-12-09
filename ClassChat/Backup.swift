a0935d26d5815b69587d1bafaf18cd04579006d8
Everaldlee Johnson <alexj2space@gmail.com>
2016-12-09 15:15:40 -0500

    Code Fixes

--- ClassChat/Backup.swift
+++ ClassChat/Backup.swift
@@ -1,22 +1,29 @@
-  override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
-    // 1
-    let itemRef = messageRef.childByAutoId()
-    
+   // 1
+   if let photoReferenceUrl = info[UIImagePickerControllerReferenceURL] as? URL {
+    // Handle picking a Photo from the Photo Library
     // 2
-    
-    let messageItem = [
-        "senderId": senderId!,
-        "senderName": senderDisplayName!,
-        "text": text!,
-        ]
+    let assets = PHAsset.fetchAssets(withALAssetURLs: [photoReferenceUrl], options: nil)
+    let asset = assets.firstObject
     
     // 3
-    itemRef.setValue(messageItem)
-    
-    // 4
-    JSQSystemSoundPlayer.jsq_playMessageSentSound()
-    
-    // 5
-    finishSendingMessage()
-    isTyping = false
-  }
+    if let key = sendPhotoMessage() {
+        // 4
+        asset?.requestContentEditingInput(with: nil, completionHandler: { (contentEditingInput, info) in
+            let imageFileURL = contentEditingInput?.fullSizeImageURL
+            
+            // 5
+            let path = "\(FIRAuth.auth()?.currentUser?.uid)/\(Int(Date.timeIntervalSinceReferenceDate * 1000))/\(photoReferenceUrl.lastPathComponent)"
+            
+            // 6
+            self.storageRef.child(path).putFile(imageFileURL!, metadata: nil) { (metadata, error) in
+                if let error = error {
+                    print("Error uploading photo: \(error.localizedDescription)")
+                    return
+                }
+                // 7
+                self.setImageURL(self.storageRef.child((metadata?.path)!).description, forPhotoMessageWithKey: key)
+            }
+        })
+    }
+   } else {
+        
--- ClassChat/ChatViewController.swift
+++ ClassChat/ChatViewController.swift
@@ -410,35 +410,7 @@ extension ChatViewController: UIImagePickerControllerDelegate, UINavigationContr
 
     picker.dismiss(animated: true, completion:nil)
 
-    // 1
-    if let photoReferenceUrl = info[UIImagePickerControllerReferenceURL] as? URL {
-      // Handle picking a Photo from the Photo Library
-      // 2
-      let assets = PHAsset.fetchAssets(withALAssetURLs: [photoReferenceUrl], options: nil)
-      let asset = assets.firstObject
-
-      // 3
-      if let key = sendPhotoMessage() {
-        // 4
-        asset?.requestContentEditingInput(with: nil, completionHandler: { (contentEditingInput, info) in
-          let imageFileURL = contentEditingInput?.fullSizeImageURL
-
-          // 5
-          let path = "\(FIRAuth.auth()?.currentUser?.uid)/\(Int(Date.timeIntervalSinceReferenceDate * 1000))/\(photoReferenceUrl.lastPathComponent)"
-
-          // 6
-          self.storageRef.child(path).putFile(imageFileURL!, metadata: nil) { (metadata, error) in
-            if let error = error {
-              print("Error uploading photo: \(error.localizedDescription)")
-              return
-            }
-            // 7
-            self.setImageURL(self.storageRef.child((metadata?.path)!).description, forPhotoMessageWithKey: key)
-          }
-        })
-      }
-    } else {
-     
+ 
         // 1
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
         // 2
@@ -461,9 +433,9 @@ extension ChatViewController: UIImagePickerControllerDelegate, UINavigationContr
             }
         }
     }
-  }
+}
 
   func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
     picker.dismiss(animated: true, completion:nil)
-  }
 }
+
