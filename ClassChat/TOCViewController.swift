0bd26b7dfbe189c2161b8cb88b3ae851e79b8756
Everaldlee Johnson <alexj2space@gmail.com>
2016-12-10 00:26:41 -0500

    code fixes and improvements :)

--- ClassChat/TOCViewController.swift
+++ ClassChat/TOCViewController.swift
@@ -12,6 +12,50 @@ import Foundation
 class TOCViewController: UIViewController {
     
     
+    @IBAction func disagreePressed(_ sender: AnyObject) {
+        let alert = UIAlertController(title: "Are you sure?",
+                                      message: "The app will quit",
+                                      preferredStyle: .alert)
+        
+        let yesAction = UIAlertAction(title: "Yes",
+                                      style: .default) { action in
+                                        exit(0)
+        }
+        let noAction = UIAlertAction(title: "No",
+                                     style: .default)
+        
+        
+        alert.addAction(yesAction)
+        alert.addAction(noAction)
+        
+        self.present(alert, animated: true, completion: nil)
+        return
+    }
+    
+    @IBAction func agreePressed(_ sender: AnyObject) {
+        let alert = UIAlertController(title: "Have you read and understood the Terms",
+                                      message: "You will be held accountable",
+                                      preferredStyle: .alert)
+        
+        let yesAction = UIAlertAction(title: "Yes",
+                                      style: .default) { action in
+                                        self.performSegue(withIdentifier: "showLogin", sender: nil)
+                                        
+        }
+        let noAction = UIAlertAction(title: "No",
+                                     style: .default)
+        
+        
+        alert.addAction(yesAction)
+        alert.addAction(noAction)
+        
+        self.present(alert, animated: true, completion: nil)
+        return
+    }
+
+
+    
+    
     
     
     
