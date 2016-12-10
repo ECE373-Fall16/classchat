fc6631e8731f9f2d9c861ca28c4f5f8e1c0d5b89
Everaldlee Johnson <alexj2space@gmail.com>
2016-12-09 23:35:47 -0500

    Added Term and Conditions View Controller

---
+++ ClassChat/TOCViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TOCViewController.swift
+//  ClassChat
+//
+//  Created by Everaldlee Johnson on 12/9/16.
+//
+//
+import UIKit
+import Foundation
+
+
+class TOCViewController: UIViewController {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
